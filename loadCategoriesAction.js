72ad64e1b520c610c773051358dcf86cd58add11
Hadjer Attatba <archiger@yahoo.fr>
2023-12-14 13:04:21 +0100

    update

--- loadCategoriesAction.js
+++ loadCategoriesAction.js
@@ -2,35 +2,30 @@
 const titlesUrl = "http://localhost:8000/api/v1/titles/";
 
 async function getListBestScores(category) {
-    // Ajoutez le filtre pour la catégorie spécifiée
-    const apiUrl = `${titlesUrl}?sort_by=-imdb_score&page_size=7&genre=${category}`;
+    // Utilisez l'ID du genre "Action" (14) dans la requête
+    const apiUrl = `${titlesUrl}?sort_by=-imdb_score&page_size=7&genre=14`;
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(listBestScores => {
-            listBestScores.results.forEach(element => {
-                const topRatedCarousel = document.querySelector("#topRatedCarousel");
+    try {
+        const response = await fetch(apiUrl);
+        const listBestScores = await response.json();
 
-                let div = document.createElement('div')
-                div.classList.add('carousel-item');
-                let img = document.createElement('img');
-                img.setAttribute('src', element['image_url'])
-                div.appendChild(img);
-                topRatedCarousel.appendChild(div);
-            });
+        // Supprimer tous les éléments actuels du carrousel
+        const topRatedCarousel = document.querySelector("#topRatedCarousel");
+        topRatedCarousel.innerHTML = '';
+
+        listBestScores.results.forEach(element => {
+            let div = document.createElement('div')
+            div.classList.add('carousel-item');
+            let img = document.createElement('img');
+            img.setAttribute('src', element['image_url'])
+            div.appendChild(img);
+            topRatedCarousel.appendChild(div);
         });
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
 }
 
 // Appeler la fonction pour afficher les films les mieux notés de la catégorie "Action"
-getListBestScores('Action');
-
-
-
-
-
-
-
-
-
-
+getListBestScores();
 
