17f80d1abbdc73bb022935442927233d929ceac0
Hadjer Attatba <archiger@yahoo.fr>
2023-12-21 17:38:27 +0100

    m

--- clickEvents.js
+++ clickEvents.js
@@ -1,9 +1,9 @@
-var images = document.querySelectorAll('img');
+document.addEventListener("DOMContentLoaded", (event) => {
+    var images = document.querySelectorAll('img');
 
-images.forEach(function(element) {
-    element.addEventListener("click", function(event) {
-        console.log(event);
+    images.forEach(function(element) {
+        element.addEventListener("click", function(event) {
+            console.log(event);
+        });
     });
 });
-
-console.log(images)
